f6335a0827a189ea1fb69333226299f789370637
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2021-02-03 16:34:15 -0300

    Criando Componente Contact

---
+++ src/components/Contact/Index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="background">
+    <form class="form-control text-center" action="/" method="POST">
+      <h1>Faça seu orçamento</h1>
+      <input type="text" class="form-control" /> <br />
+      <input type="text" class="form-control" /> <br />
+      <input type="text" class="form-control" /> <br />
+      <input type="text" class="form-control" /> <br />
+      <button class="btn" type="submit">Enviar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.background {
+  background: url('../../assets/img/contact.png') fixed;
+  background-size: 100%;
+  height: 60vh;
+  form{
+    height: 60vh;
+    background: transparent;
+    input {
+      width: 50%;
+      margin: 10px auto;
+    }
+  }
+}
+</style>
