efee634c4c52109dcf3fd9f40d99ddfe207bd894
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2021-02-03 16:30:56 -0300

    Adicionando vue-scrollto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueScrollTo from 'vue-scrollto'
 
 import 'animate.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 const app = createApp(App)
+app.use(VueScrollTo)
 app.use(router)
 app.mount('#app')
