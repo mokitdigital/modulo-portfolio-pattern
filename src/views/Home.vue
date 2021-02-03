9868c3f097441e8cb76c039c1365c9a666a9afd0
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2021-02-03 16:34:32 -0300

    Importando Components para Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <Header />
+    <Photos id="album" />
+    <Contact id="contact" />
     <Footer />
   </div>
 </template>
@@ -8,12 +10,16 @@
 <script>
 // @ is an alias to /src
 import Header from '@/components/Header/Index.vue'
+import Photos from '@/components/Photos/Index.vue'
+import Contact from '@/components/Contact/Index.vue'
 import Footer from '@/components/Footer/Index.vue'
 
 export default {
   name: 'Home',
   components: {
     Header,
+    Photos,
+    Contact,
     Footer
   }
 }
